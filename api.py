2087a721864bdfcb62c90b5a589e3689362001fd
Aditya YV <adityayv802@gmail.com>
2024-11-17 01:03:10 -0600

    List enpoints

--- api.py
+++ api.py
@@ -1,5 +1,9 @@
 from fastapi import FastAPI
+import glob
 from fastapi.middleware.cors import CORSMiddleware
+import os
+
+import pandas as pd
 
 app = FastAPI()
 
@@ -11,9 +15,22 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+temp_csv_files = glob.glob('datasets_chosen/*.csv')
+csv_files = [os.path.basename(file) for file in temp_csv_files]
+
 @app.get("/")
 def main_page():
     return "Hello World"
+
+@app.get("/listdatasets")
+def list_csv_files():
+    return csv_files
+
+for endpoint in csv_files:
+    @app.get(f"/{endpoint}")
+    def dynamic_endpoint(endpoint = endpoint):
+        data = pd.read_csv(f"datasets_chosen/{endpoint}")
+        return data.to_dict()
 
 if __name__ == "__main__":
     import uvicorn
